ca88a52630c8aa044408518cb080e7f44eafa7cd
Tiefeng Wu <icebergwtf@qq.com>
2015-04-27 12:19:22 +0800

    setup.sh update

--- setup.sh
+++ setup.sh
@@ -11,10 +11,11 @@ git submodule foreach git pull origin
 
 if [ -d ~/.vim_runtime ]
 then
-    cd ~/.vim_runtime
-	git pull --rebase
+  cd ~/.vim_runtime
+  git pull --rebase
 else
-	git clone git://github.com/amix/vimrc.git ~/.vim_runtime
+  git clone --recursive git://github.com/amix/vimrc.git ~/.vim_runtime
 fi
+
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 ln -sf ~/.myvim/.vimrc ~/.vim_runtime/my_configs.vim
