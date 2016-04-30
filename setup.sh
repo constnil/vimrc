a4977d53c203489864d968ff2d1db8406bff51da
Tiefeng Wu <icebergwtf@qq.com>
2016-04-30 03:26:53 +0000

    setup.sh add git gc call

--- setup.sh
+++ setup.sh
@@ -2,19 +2,23 @@
 
 cd ~/.myvim
 git pull
+git gc
 
 git submodule init
 git submodule sync
 git submodule update
 git submodule foreach git checkout master
-git submodule foreach git pull origin
+git submodule foreach git pull -f origin
+git submodule foreach git gc
 
 if [ -d ~/.vim_runtime ]
 then
   cd ~/.vim_runtime
   git pull --rebase
+  git gc
 else
   git clone --recursive git://github.com/amix/vimrc.git ~/.vim_runtime
+  git gc
 fi
 
 sh ~/.vim_runtime/install_awesome_vimrc.sh
